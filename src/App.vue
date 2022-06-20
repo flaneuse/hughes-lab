bde71f5e8ae0e93a1154461a7d8ad617f33693fc
flaneuse <flaneuse@users.noreply.github.com>
2022-06-20 15:12:25 -0700

    initial cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+<div id="app">
+  <div id="nav">
+    <router-link to="/">home</router-link> |
+    <router-link :to="link.url" class="mr-2" v-for="(link, lIdx) in links" :key="lIdx">{{link.name}}</router-link>
   </div>
+  <router-view />
+</div>
 </template>
+
+<script>
+// import Vue from "vue";
+export default {
+  name: "LocationReportComponent",
+  data() {
+    return ({
+      links: [{
+          name: "research",
+          url: "/research"
+        },
+        {
+          name: "people",
+          url: "/people"
+        },
+        {
+          name: "publications",
+          url: "/publications"
+        },
+        {
+          name: "code",
+          url: "/code"
+        },
+        {
+          name: "news",
+          url: "/news"
+        }
+      ]
+    })
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
